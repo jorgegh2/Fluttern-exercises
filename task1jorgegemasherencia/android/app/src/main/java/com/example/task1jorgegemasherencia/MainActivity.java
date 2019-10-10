c5e9e076518f2d47033b82df9d70d541831df43e
Jorge Gemas Herencia <jorgegh2@TR12.CAT>
2019-10-10 16:58:33 +0200

    project task1 created

---
+++ task1jorgegemasherencia/android/app/src/main/java/com/example/task1jorgegemasherencia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.task1jorgegemasherencia;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
